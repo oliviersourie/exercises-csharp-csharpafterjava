3b437de49ff8569aaf4acf3c6b5fbd1f249674f9
Olivier Sourie <Olivier.Sourie@howest.be>
2020-02-16 20:37:34 +0100

    Init

--- ConsoleApp/Factorial.cs
+++ ConsoleApp/Factorial.cs
@@ -4,24 +4,12 @@ namespace ConsoleApp
 {
     class Factorial
     {
-        public int Value { get; set; }
-
-        public Factorial(int? startValue)
+        public static string Calculate(int startValue = 1)
         {
-            Value = startValue ?? 0; //null coalescing
+            return $"De faculteit van {startValue} is {FacCount(startValue)}";
         }
 
-        public Factorial() : this(null)
-        {
-
-        }
-
-        public string Calculate()
-        {
-            return $"De faculteit van {Value} is {FacCount(Value)}";
-        }
-
-        private int FacCount(int v)
+        private static int FacCount(int v)
         {
             if (v > 0)
             {
