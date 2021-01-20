ebfb4e03bd16577f3aae1e254c7863b938f6f2fa
Olivier Sourie <Olivier.Sourie@howest.be>
2021-01-20 16:33:43 +0100

    Init Migration to .NET5

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp
 {
@@ -6,7 +7,24 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            foreach (int i in Fibonacci(20))
+            {
+                Console.WriteLine(i);
+            }
+
+            static ICollection<int> Fibonacci(int range) //local function
+            {
+                int current = 1, next = 1;
+                ICollection<int> fibonacciList = new List<int>();
+
+                for (int counter = 0; counter < range; counter++)
+                {
+                    fibonacciList.Add(current);
+                    next = current + (current = next);
+
+                }
+                return fibonacciList;
+            }
         }
     }
 }
