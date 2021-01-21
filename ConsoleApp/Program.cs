1ec74a14282714fc8f8684b88ba11339f6d2254b
Olivier Sourie <Olivier.Sourie@howest.be>
2021-01-21 16:41:07 +0100

    Some LinQ code testing

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             const string stop = "STOP";
-            string inputText = string.Empty;
+            string inputText;
             Dictionary<string, int> frequencyTable = new();
 
             do
@@ -34,10 +34,9 @@ namespace ConsoleApp
             }
 
             //with LINQ: you can leave out the test on 'stop' in the do...while above
-            //foreach (KeyValuePair<string, int> text in frequencyTable.Where(x => x.Key is not stop))
-            //{
-            //    Console.WriteLine($"{text.Key}: {text.Value}");
-            //}
+            //frequencyTable.Where(text => text.Key is not stop)
+            //              .ToList()
+            //              .ForEach(text => Console.WriteLine($"{text.Key}: {text.Value}"));
 
         }
     }
