4cd80ff164049b4eee92e94019e1a7eb8e54c064
Olivier Sourie <Olivier.Sourie@howest.be>
2021-01-21 15:58:13 +0100

    Init Migrate to .NET5

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleApp
 {
@@ -6,7 +8,37 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string stop = "STOP";
+            string inputText = string.Empty;
+            Dictionary<string, int> frequencyTable = new();
+
+            do
+            {
+                inputText = Console.ReadLine();
+                if (inputText != stop)
+                {
+                    if (frequencyTable.TryGetValue(inputText, out int currentValue))
+                    {
+                        frequencyTable[inputText] = ++currentValue;
+                    }
+                    else
+                    {
+                        frequencyTable.Add(inputText, 1);
+                    }
+                }
+            } while (inputText != stop);
+
+            foreach (KeyValuePair<string, int> text in frequencyTable)
+            {
+                Console.WriteLine($"{text.Key}: {text.Value}");
+            }
+
+            //with LINQ: you can leave out the test on 'stop' in the do...while above
+            //foreach (KeyValuePair<string, int> text in frequencyTable.Where(x => x.Key is not stop))
+            //{
+            //    Console.WriteLine($"{text.Key}: {text.Value}");
+            //}
+
         }
     }
 }
