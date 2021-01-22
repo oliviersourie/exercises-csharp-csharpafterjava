2def3e072d45192d341141431350e0ee6d6af41f
Olivier Sourie <Olivier.Sourie@howest.be>
2021-01-22 14:34:13 +0100

    Add Enum types

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,4 +9,37 @@ namespace ConsoleApp
             Console.WriteLine("Hello World!");
         }
     }
+
+    enum Show
+    {
+        Numbers,
+        Names
+    }
+
+    [Flags]
+    enum WeekDays: byte
+    {
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 4,
+        Thursday = 8,
+        Friday = 16,
+        Saturday = 32,
+        Sunday = 64,
+        Midweek = Monday | Tuesday | Wednesday | Thursday | Friday,
+        Weekdays = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday,
+        Weekend = Saturday | Sunday
+    }
+
+    class Calendar
+    {
+        public void ShowDays()
+        {
+            //To do
+        }
+        public void AddEventDay()
+        {
+            //To do
+        }
+    }
 }
